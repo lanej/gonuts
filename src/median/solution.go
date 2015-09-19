b44f925476b1363473037acb6affba79de0dc36a
Josh Lane <me@joshualane.com>
2015-09-19 14:35:44 -0700

    solution!

--- src/median/solution.go
+++ src/median/solution.go
@@ -7,21 +7,49 @@ import (
 	"strings"
 )
 
-func solve(r *os.File, w *os.File) {
-	reader := bufio.NewReader(r)
-
-	num_s, _ := reader.ReadString('\n')
-	w.WriteString(num_s)
-	num, err := strconv.ParseInt(strings.Trim(num_s, "\n"), 10, 64)
+func nextNumber(r *bufio.Reader) float64 {
+	num_s, _ := r.ReadString('\n')
+	num, err := strconv.ParseFloat(strings.Trim(num_s, "\n"), 64)
 
 	if nil != err {
 		panic(err)
 	}
 
-	w.WriteString("something\n")
-	w.WriteString(strconv.FormatInt(num, 10))
-	w.WriteString("\n")
-	w.WriteString("something else\n")
+	return num
+}
+
+func median(s *[]float64) float64 {
+	sequence := *s
+	size := len(*s)
+	middle := (size / 2)
+
+	if size%2 == 0 {
+		low_middle := sequence[middle-1]
+		high_middle := sequence[middle]
+
+		return (high_middle + low_middle) / 2.0
+	} else {
+
+		return sequence[middle]
+	}
+}
+
+func solve(r *os.File, w *os.File) {
+	reader := bufio.NewReader(r)
+
+	count := int(nextNumber(reader))
+	var sequence []float64
+
+	for i := 0; i < count; i++ {
+		sequence = append(sequence, nextNumber(reader))
+		w.WriteString(strconv.FormatFloat(median(&sequence), 'f', 1, 32))
+
+		if (i + 1) < count {
+			w.WriteString("\n")
+		}
+	}
+
+	// only exit after correct number of lines received
 }
 
 func main() {
--- src/median/solution_test.go
+++ src/median/solution_test.go
@@ -1,6 +1,7 @@
 package median
 
 import (
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -10,6 +11,7 @@ func TestSolve(t *testing.T) {
 	dir := os.TempDir()
 	input_file, _ := ioutil.TempFile(dir, "median")
 
+	// rewite as a go routine
 	input_file.WriteString("10\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10")
 	input_file.Seek(0, 0)
 
@@ -19,6 +21,7 @@ func TestSolve(t *testing.T) {
 
 	output_bytes, _ := ioutil.ReadFile(output_file.Name())
 
-	t.Logf(string(output_bytes[:len(output_bytes)]))
-	t.Logf("starts")
+	output := string(output_bytes[:len(output_bytes)])
+	expected_output := "1.0\n1.5\n2.0\n2.5\n3.0\n3.5\n4.0\n4.5\n5.0\n5.5"
+	assert.Equal(t, expected_output, output)
 }
