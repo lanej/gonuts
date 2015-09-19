b25fe1c68afca1a7e1d3b7ed5ed41ebf72b75bbc
Josh Lane <me@joshualane.com>
2015-09-19 14:58:33 -0700

    remove sequence, need heap for perf

--- src/median/solution.go
+++ src/median/solution.go
@@ -8,19 +8,6 @@ import (
 	"strings"
 )
 
-type Sequence []float64
-
-func (s Sequence) Len() int {
-	return len(s)
-}
-
-func (s Sequence) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s Sequence) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
 func nextNumber(r *bufio.Reader) float64 {
 	num_s, _ := r.ReadString('\n')
 	num, err := strconv.ParseFloat(strings.Trim(num_s, "\n"), 64)
