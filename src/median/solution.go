1ec3adfbe21c9c67ba23562e5fd358d13c39e976
Josh Lane <me@joshualane.com>
2015-09-19 17:55:47 -0700

    heaping

--- src/median/solution.go
+++ src/median/solution.go
@@ -2,15 +2,57 @@ package median
 
 import (
 	"bufio"
+	"container/heap"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-func nextNumber(r *bufio.Reader) float64 {
+type MinHeap []int64
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int64))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type MaxHeap []int64
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MaxHeap) Root() int64        { return h[0] }
+func (h MinHeap) Root() int64        { return h[0] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int64))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func nextNumber(r *bufio.Reader) int64 {
 	num_s, _ := r.ReadString('\n')
-	num, err := strconv.ParseFloat(strings.Trim(num_s, "\n"), 64)
+	num, err := strconv.ParseInt(strings.Trim(num_s, "\n"), 10, 64)
 
 	if nil != err {
 		panic(err)
@@ -19,32 +61,36 @@ func nextNumber(r *bufio.Reader) float64 {
 	return num
 }
 
-func median(s *[]float64) float64 {
-	sequence := *s
-	size := len(*s)
-	middle := (size / 2)
-
-	if size%2 == 0 {
-		low_middle := sequence[middle-1]
-		high_middle := sequence[middle]
-
-		return (high_middle + low_middle) / 2.0
-	} else {
-
-		return sequence[middle]
-	}
-}
-
 func solve(r *os.File, w *os.File) {
 	reader := bufio.NewReader(r)
 
 	count := int(nextNumber(reader))
-	var sequence []float64
+	minHeap := &MinHeap{}
+	maxHeap := &MaxHeap{}
+
+	heap.Init(minHeap)
+	heap.Init(maxHeap)
+
+	var median float64
 
 	for i := 0; i < count; i++ {
-		sequence = append(sequence, nextNumber(reader))
-		sort.Float64s(sequence)
-		w.WriteString(strconv.FormatFloat(median(&sequence), 'f', 1, 32))
+		num := int64(nextNumber(reader))
+
+		if minHeap.Len() == 0 || (maxHeap.Len() > minHeap.Len()) {
+			heap.Push(minHeap, num)
+		} else {
+			heap.Push(maxHeap, num)
+		}
+
+		if maxHeap.Len() == minHeap.Len() {
+			median = (float64(maxHeap.Root()) + float64(minHeap.Root())) / 2
+		} else if maxHeap.Len() > minHeap.Len() {
+			median = float64(maxHeap.Root())
+		} else {
+			median = float64(minHeap.Root())
+		}
+
+		w.WriteString(strconv.FormatFloat(median, 'f', 1, 32))
 
 		if (i + 1) < count {
 			w.WriteString("\n")
